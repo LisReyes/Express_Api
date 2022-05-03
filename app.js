aa0283d513010ee45dc3634d2d85ea73cd29d2fd
LisReyes <launchx10399@innovaccion.mx>
2022-05-02 20:46:13 -0500

    Estoy añadiendo la parte de GET que utilizamos para obetener los atributos

--- app.js
+++ app.js
@@ -6,7 +6,18 @@ app.use(express.json())//Indicamos que usaremos JSON
 const port = 3000
 
 //routes
-
+//Methods HTTP
+app.get('/v1/explorers/',(req, res) =>{
+    console.log(`GET EXPLORERS V1 API ${new Date()}`)
+    const explorer1 = {id: 1, name: "Lis1"}
+    const explorer2 = {id: 2, name: "Lis2"}
+    const explorer3 = {id: 3, name: "Lis3"}
+    const explorer4 = {id: 4, name: "Lis4"}
+    const explorer5 = {id: 5, name: "Lis5"}
+    const explorers = [explorer1,explorer2,explorer3,explorer4,explorer5]
+    //HTTP CODE STATUS: 200
+    res.status(200).json(explorers)//nos indicara el error que tiene
+})
 
 //con sto inicializamos la app
 app.listen(port,()=>{
